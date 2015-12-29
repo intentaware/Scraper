fad87da2ade897724ec2f896a78340f5e98c3b31
Shalini Roy <shaliniroy012@gmail.com>
2015-12-29 18:29:34 +0530

    Added code for scraping whole website

--- intentscraper/intentscraper/spiders/intentscraper_spider.py
+++ intentscraper/intentscraper/spiders/intentscraper_spider.py
@@ -1,24 +1,46 @@
+import re
 import sys
 import scrapy
 
 from intentscraper.items import IntentscraperItem
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.selector import HtmlXPathSelector
+from scrapy.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.spiders import BaseSpider, Spider
+from urlparse import urljoin
 
 
-class DmozSpider(scrapy.Spider):
+class DmozSpider(CrawlSpider):
     name = "intentscraper"
-    allowed_domains = ["*"]
+    # allowed_domains = ["phpmongotweet-portalresearch.rhcloud.com"]
+    # rules = (Rule(SgmlLinkExtractor(), callback='parse_items', follow=True),)
+    # start_urls = ['http://phpmongotweet-portalresearch.rhcloud.com/']
 
     def __init__(self, *args, **kwargs):
         super(DmozSpider, self).__init__(*args, **kwargs)
         urls = kwargs.get('urls')
+        domain = kwargs.get('domain')
         self.start_urls = [urls]
+        self.allowed_domains = [domain]
+        self.crawledLinks = []
 
     def parse(self, response):
-        print sys.argv
-        for sel in response.xpath('//ul/li'):
-            item = IntentscraperItem()
-            item['title'] = sel.xpath('a/text()').extract()
-            item['link'] = sel.xpath('a/@href').extract()
-            item['desc'] = sel.xpath('text()').extract()
-            yield item
+        hxs = scrapy.Selector(response)
+        links = hxs.xpath("//a/@href").extract()
+
+        for n, i in enumerate(links):
+            links[n]= urljoin(self.start_urls[0], i)
+
+        linkPattern = re.compile("^(?:ftp|http|https):\/\/(?:[\w\.\-\+]+:{0,1}[\w\.\-\+]*@)?(?:[a-z0-9\-\.]+)(?::[0-9]+)?(?:\/|\/(?:[\w#!:\.\?\+=&amp;%@!\-\/\(\)]+)|\?(?:[\w#!:\.\?\+=&amp;%@!\-\/\(\)]+))?$")
+        print('A response from %s just arrived!' % response.url)
+        for link in links:
+            # If it is a proper link and is not checked yet, yield it to the Spider
+            if linkPattern.match(link) and not link in self.crawledLinks:
+                self.crawledLinks.append(link)
+                yield scrapy.Request(link, callback = self.parse)
+
+    def parse_category(self, response):
+        filename = response.url.split("/")[-1] + '.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
 
