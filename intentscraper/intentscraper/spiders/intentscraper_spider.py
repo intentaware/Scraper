083c46456bfa8d5aa7af129d04de9f5f75de0a46
Shalini Roy <shaliniroy012@gmail.com>
2015-12-30 16:50:13 +0530

    Added code to parse raw data, script links, style links and corresponding url

--- intentscraper/intentscraper/spiders/intentscraper_spider.py
+++ intentscraper/intentscraper/spiders/intentscraper_spider.py
@@ -10,26 +10,36 @@ from scrapy.spiders import BaseSpider, Spider
 from urlparse import urljoin
 
 
-class DmozSpider(CrawlSpider):
+class IntentSpider(CrawlSpider):
     name = "intentscraper"
     # allowed_domains = ["phpmongotweet-portalresearch.rhcloud.com"]
     # rules = (Rule(SgmlLinkExtractor(), callback='parse_items', follow=True),)
     # start_urls = ['http://phpmongotweet-portalresearch.rhcloud.com/']
 
     def __init__(self, *args, **kwargs):
-        super(DmozSpider, self).__init__(*args, **kwargs)
+        super(IntentSpider, self).__init__(*args, **kwargs)
         urls = kwargs.get('urls')
         domain = kwargs.get('domain')
         self.start_urls = [urls]
         self.allowed_domains = [domain]
         self.crawledLinks = []
 
+
     def parse(self, response):
         hxs = scrapy.Selector(response)
         links = hxs.xpath("//a/@href").extract()
-
+        crawledUrl = response.url
+        rawData = response.body
+        scriptLinks = response.xpath('//script/@src').extract()
+        for n, i in enumerate(scriptLinks):
+            scriptLinks[n] = urljoin(self.start_urls[0], i)
+        styleLinks = response.xpath('//link[@rel="stylesheet"]/@href').extract()
+        for n, i in enumerate(styleLinks):
+            styleLinks[n] = urljoin(self.start_urls[0], i)
+        data = {'url':crawledUrl,'raw':rawData, 'scripts':scriptLinks, 'styles':styleLinks, 'analysis': 'to be done later', 'tags':'to be done later'}
+        print data
         for n, i in enumerate(links):
-            links[n]= urljoin(self.start_urls[0], i)
+            links[n] = urljoin(self.start_urls[0], i)
 
         linkPattern = re.compile("^(?:ftp|http|https):\/\/(?:[\w\.\-\+]+:{0,1}[\w\.\-\+]*@)?(?:[a-z0-9\-\.]+)(?::[0-9]+)?(?:\/|\/(?:[\w#!:\.\?\+=&amp;%@!\-\/\(\)]+)|\?(?:[\w#!:\.\?\+=&amp;%@!\-\/\(\)]+))?$")
         print('A response from %s just arrived!' % response.url)
@@ -43,4 +53,5 @@ class DmozSpider(CrawlSpider):
         filename = response.url.split("/")[-1] + '.html'
         with open(filename, 'wb') as f:
             f.write(response.body)
+
 
