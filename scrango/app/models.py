9210a010704fec6ca28dc9e99901997af89cc052
Shalini Roy <shaliniroy012@gmail.com>
2016-01-11 01:40:48 +0530

    Removed scrango project

--- scrango/app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from model_utils.models import TimeStampedModel
-
-
-class ScrapedData(TimeStampedModel):
-    domain = models.CharField(max_length=255, null=True, blank=True)
-    url = models.TextField(null=True, blank=True)
-    jsondata = models.TextField(null=True, blank=True)
